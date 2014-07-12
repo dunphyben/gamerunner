a6dc6ee6551553211f5868f67ff887f08a183453
nancywong <nancypwong@gmail.com>
2014-07-12 16:15:58 -0700

    saved file

--- gamerunner/GameScene.swift
+++ gamerunner/GameScene.swift
@@ -22,45 +22,38 @@ class GameScene: SKScene {
         runner.position = CGPoint(x: 100, y: 100);
         self.addChild(runner);  //adds the sprite to the scene for drawing
         
-//        /* Setup your scene here */
-//        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-//        myLabel.text = "Hello, World!";
-//        myLabel.fontSize = 65;
-//        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
-//        
-//        self.addChild(myLabel)
-        
     }
     
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
         
         for touch : AnyObject in touches {
             let location = touch.locationInNode(self)
-            runner.xScale *= 1.1;
-            runner.yScale *= 1.1;
+            let skView = self.view as SKView
+            skView.ignoresSiblingOrder = true
+            var scene = PlayScene(size: self.size)
+            scene.scaleMode = .ResizeFill
+            scene.size = skView.bounds.size
+            skView.presentScene(scene)
         }
-        
-        /* Called when a touch begins */
-        
-//        for touch: AnyObject in touches {
-//            let location = touch.locationInNode(self)
-//            
-//            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-//            
-//            sprite.xScale = 0.5
-//            sprite.yScale = 0.5
-//            sprite.position = location
-//            
-//            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-//            
-//            sprite.runAction(SKAction.repeatActionForever(action))
-//            
-//            self.addChild(sprite)
-//        }
+
     }
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
-        runner.runAction(SKAction.rotateByAngle(CGFloat(M_PI/8), duration:1))
+    }
+}
+
+class BlockStatus {
+    var isRunning = false
+    var untilNextRun = UInt32(0)
+    var currentGap = UInt32(0)
+    init(isRunning:Bool, untilNextRun:UInt32, currentGap:UInt32) {
+        self.isRunning = isRunning
+        self.untilNextRun = untilNextRun
+        self.currentGap = currentGap
+    }
+    
+    func shouldRunBlock() -> Bool {
+        return self.currentGap > self.untilNextRun
     }
 }
--- gamerunner/PlayScene.swift
+++ gamerunner/PlayScene.swift
@@ -6,4 +6,45 @@
 //  Copyright (c) 2014 HackRunner Inc. All rights reserved.
 //
 
-import Foundation
+import SpriteKit
+
+class PlayScene: SKScene, SKPhysicsContactDelegate {
+    let runner = SKSpriteNode(imageNamed:"runner")
+    let bar = SKSpriteNode(imageNamed:"bottomBar")
+    let blockSmall = SKSpriteNode(imageNamed:"block1")
+    let blockBig = SKSpriteNode(imageNamed:"block2")
+    
+    var barStartX = CGFloat(0)
+    var maxBarX = CGFloat(0)
+    var groundSpeed = 5
+    var runnerBaseline = CGFloat(0)
+    var onGround = true
+    var yVelocity = CGFloat(0)
+    let gravity = CGFloat(0.6)
+    
+    var blockMaxX = CGFloat(0)
+    var blockStartX = CGFloat(0)
+    
+    enum ColliderType:UInt32 {
+        case Runner = 1
+        case Block = 2
+    }
+    
+    override func didMoveToView(view: SKView!) {
+        self.physicsWorld.contactDelegate = self
+        
+        // bottomBar positioning
+        self.bar.anchorPoint = CGPointMake(0, 0.5)
+        self.bar.position = CGPointMake(
+            CGRectGetMinX(self.frame),
+            CGRectGetMinY(self.frame) + (self.bar.size.height / 2))
+        self.barStartX = self.bar.position.x
+        self.maxBarX = self.bar.size.width - self.frame.size.width
+        self.maxBarX *= -1
+        
+        // runner positioning
+        self.runnerBaseline = self.bar.position.y + (self.bar.size.height / 2) +
+            (self.runner.size.height / 2)
+        
+    }
+}
