4e5173010f6f5c870982e5808a1822ee18b316f0
nancywong <nancypwong@gmail.com>
2014-07-12 17:20:57 -0700

    physics added for blocks and runner

--- gamerunner/GameScene.swift
+++ gamerunner/GameScene.swift
@@ -8,38 +8,160 @@
 
 import SpriteKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
-    let _runnerWidth = 75;
-    let _runnerHeight = 50;
+    let runner = SKSpriteNode(imageNamed:"runner")
+    let bar = SKSpriteNode(imageNamed:"bottomBar")
+    let blockSmall = SKSpriteNode(imageNamed:"blockSmall")
+    let blockBig = SKSpriteNode(imageNamed:"blockBig")
     
-    let runner:SKSpriteNode = SKSpriteNode(imageNamed: "runner.png")
+    var barStartX = CGFloat(0)
+    var maxBarX = CGFloat(0)
+    var groundSpeed = 5
+    var runnerBaseline = CGFloat(0)
+    var onGround = true
+    var yVelocity = CGFloat(0)
+    let gravity = CGFloat(0.6)
     
-    override func didMoveToView(view: SKView) {
-        runner.size = CGSize(width: _runnerWidth, height: _runnerHeight)
-        runner.zPosition = 100;
-        runner.anchorPoint = CGPoint(x: 0.5, y: 0.5)
-        runner.position = CGPoint(x: 100, y: 100);
-        self.addChild(runner);  //adds the sprite to the scene for drawing
-        
+    var blockMaxX = CGFloat(0)
+    var blockStartX = CGFloat(0)
+    
+    enum ColliderType:UInt32 {
+        case Runner = 1
+        case Block = 2
     }
     
-    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+    override func didMoveToView(view: SKView!) {
+        self.physicsWorld.contactDelegate = self
         
-        for touch : AnyObject in touches {
-            let location = touch.locationInNode(self)
-            let skView = self.view as SKView
-            skView.ignoresSiblingOrder = true
-            var scene = PlayScene(size: self.size)
-            scene.scaleMode = .ResizeFill
-            scene.size = skView.bounds.size
-            skView.presentScene(scene)
+        // bottomBar positioning
+        self.bar.anchorPoint = CGPointMake(0,0)
+        self.bar.position = CGPointMake(
+            CGRectGetMinX(self.frame),
+            CGRectGetMinY(self.frame) + (self.bar.size.height / 2))
+        self.barStartX = self.bar.position.x
+        self.maxBarX = self.bar.size.width - self.frame.size.width
+        self.maxBarX *= -1
+        
+        // runner positioning
+        self.runner.xScale *= 0.3
+        self.runner.yScale *= 0.3
+        self.runnerBaseline = self.bar.position.y + (self.bar.size.height / 2) +
+            (self.runner.size.height / 2)
+        self.runner.position = CGPointMake(CGRectGetMinX(self.frame)
+            + self.runner.size.width
+            + (self.runner.size.width/4), self.runnerBaseline)
+        self.runner.physicsBody = SKPhysicsBody(circleOfRadius: CGFloat(self.runner.size.width / 2))
+        self.runner.physicsBody.affectedByGravity = false
+        self.runner.physicsBody.categoryBitMask = ColliderType.Runner.toRaw()
+        self.runner.physicsBody.contactTestBitMask = ColliderType.Block.toRaw()
+        self.runner.physicsBody.collisionBitMask = ColliderType.Block.toRaw()
+        
+        // block 1 
+        self.blockSmall.xScale *= 0.5
+        self.blockSmall.yScale *= 0.5
+        self.blockSmall.anchorPoint = CGPointMake(0,0)
+        self.blockSmall.position = CGPointMake(CGRectGetMaxX(self.frame)
+            + self.blockSmall.size.width, self.runnerBaseline)
+        self.blockSmall.physicsBody = SKPhysicsBody(rectangleOfSize: self.blockSmall.size)
+        self.blockSmall.physicsBody.dynamic = false
+        self.blockSmall.physicsBody.categoryBitMask = ColliderType.Block.toRaw()
+        self.blockSmall.physicsBody.contactTestBitMask = ColliderType.Runner.toRaw()
+        self.blockSmall.physicsBody.collisionBitMask = ColliderType.Runner.toRaw()
+        
+        // block 2 positioning
+        self.blockBig.xScale *= 0.5
+        self.blockBig.yScale *= 0.5
+        self.blockBig.anchorPoint = CGPointMake(0,0)
+        self.blockBig.position = CGPointMake(CGRectGetMaxX(self.frame)
+            + self.blockBig.size.width, self.runnerBaseline
+                + (self.blockSmall.size.height / 2))
+        self.blockBig.physicsBody = SKPhysicsBody(rectangleOfSize: self.blockSmall.size)
+        self.blockBig.physicsBody.dynamic = false
+        self.blockBig.physicsBody.categoryBitMask = ColliderType.Block.toRaw()
+        self.blockBig.physicsBody.contactTestBitMask = ColliderType.Runner.toRaw()
+        self.blockBig.physicsBody.collisionBitMask = ColliderType.Runner.toRaw()
+        
+        self.blockStartX = self.blockSmall.position.x
+        
+        self.blockSmall.name = "blockSmall"
+        self.blockBig.name = "blockBig"
+        
+        blockStatuses["blockSmall"] = BlockStatus(isRunning: false, untilNextRun: random(), currentGap: UInt32(0))
+        blockStatuses["blockBig"] = BlockStatus(isRunning: false, untilNextRun: random(), currentGap: UInt32(0))
+        
+        self.blockMaxX = 0 - self.blockSmall.size.width / 2
+        
+        self.addChild(self.bar)
+        self.addChild(self.runner)
+        self.addChild(self.blockSmall)
+        self.addChild(self.blockBig)
+    }
+    
+    func random() -> UInt32 {
+        var range = UInt32(50)..<UInt32(200)
+        return range.startIndex + arc4random_uniform(range.endIndex - range.startIndex + 1)
+    }
+    
+    var blockStatuses:Dictionary<String,BlockStatus> = [:]
+    
+    
+    override func touchesBegan(touches: NSSet!, withEvent event: UIEvent!) {
+        if self.onGround {
+            self.yVelocity = -18.0
+            self.onGround = false
         }
-
     }
-   
-    override func update(currentTime: CFTimeInterval) {
-        /* Called before each frame is rendered */
+    
+    override func touchesEnded(touches: NSSet!, withEvent event: UIEvent!) {
+        if self.yVelocity < -9.0 {
+            self.yVelocity = -9.0
+        }
+    }
+    
+    override func update(currentTime: NSTimeInterval) {
+        if self.bar.position.x <= maxBarX {
+            self.bar.position.x = self.barStartX
+        }
+        
+        // jump code
+        self.yVelocity += self.gravity
+        self.runner.position.y -= yVelocity
+        
+        if self.runner.position.y < self.runnerBaseline {
+            self.runner.position.y = self.runnerBaseline
+            yVelocity = 0.0
+            self.onGround = true
+        }
+        
+        // TO-DO: animate the hero
+        
+        // ground scrolling
+        bar.position.x -= CGFloat(self.groundSpeed)
+        
+        blockRunner()
+    }
+    
+    func blockRunner() {
+        for(block, blockStatus) in self.blockStatuses {
+            var myBlock = self.childNodeWithName(block)
+            if blockStatus.shouldRunBlock() {
+                blockStatus.untilNextRun = random()
+                blockStatus.currentGap = 0
+                blockStatus.isRunning = true
+            }
+            
+            if blockStatus.isRunning {
+                if myBlock.position.x > blockMaxX {
+                    myBlock.position.x -= CGFloat(self.groundSpeed)
+                } else {
+                    myBlock.position.x = self.blockStartX
+                    blockStatus.isRunning = false
+                }
+            } else {
+                blockStatus.currentGap++
+            }
+        }
     }
 }
 
